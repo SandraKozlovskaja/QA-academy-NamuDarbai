22ee6cdb3aa07bc888ab0f50692401317ca1bfcc
SandraKozlovskaja <kosandra97@gmail.com>
2021-08-30 01:09:22 +0300

    Add files via upload

---
+++ NamuDarbai3/NamuDarbai3Page.cs
@@ -0,0 +1,48 @@
+﻿using autotests.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace TestPageTemplate.Page
+{
+    public class NamuDarbai3Page : BasePage
+    {
+        private const string AddressUrl = "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+        private IWebElement firstInputField => Driver.FindElement(By.Id("sum1"));
+        private IWebElement secondInputField => Driver.FindElement(By.Id("sum2"));
+        private IWebElement GetTotalButton => Driver.FindElement(By.CssSelector("#gettotal > button"));
+        private IWebElement actualResult => Driver.FindElement(By.Id("displayvalue"));
+        public NamuDarbai3Page(IWebDriver webdriver) : base(webdriver) { }
+
+        public void NavigateToPage()
+        {
+            if (Driver.Url != AddressUrl)
+            {
+                Driver.Url = AddressUrl;
+            }
+        }
+
+
+        public void ClearAndInputFirstField(string firstInput)
+        {
+            firstInputField.Clear();
+            firstInputField.SendKeys(firstInput);
+        }
+        public void ClearAndInputSecondField(string secondInput)
+        {
+            secondInputField.Clear();
+            secondInputField.SendKeys(secondInput);
+        }
+
+        public void ClickGetTotal()
+        {
+            GetTotalButton.Click();
+        }
+
+        public void VerifyResult(string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, actualResult.Text, $"Actual result differs from expected");
+        }
+
+    }
+
+}
---
+++ NamuDarbai3/NamuDarbai3Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TestPageTemplate.Page;
+
+namespace TestPageTemplate.Test
+{
+    public class NamuDarbai3Test
+    {
+        private static NamuDarbai3Page page;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            page = new NamuDarbai3Page(chrome);
+            chrome.Manage().Window.Maximize();
+            chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            page.CloseBrowser();
+        }
+
+        [TestCase("2", "2", "4", TestName = "2 plus 2 = 4")]
+        [TestCase("-5", "3", "-2", TestName = "-5 plus 3 = -2")]
+        [TestCase("a", "b", "NaN", TestName = "a plus b = NaN")]
+
+        public static void TestSum(string firstInput, string secondInput, string expectedResult)
+        {
+            page.NavigateToPage();
+            page.ClearAndInputFirstField(firstInput);
+            page.ClearAndInputSecondField(secondInput);
+            page.ClickGetTotal();
+            page.VerifyResult(expectedResult);
+        }
+    }
+}
