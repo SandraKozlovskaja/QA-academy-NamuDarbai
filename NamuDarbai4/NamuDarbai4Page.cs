25680cb816c56a002c294cbc865f60498dc37c4c
SandraKozlovskaja <kosandra97@gmail.com>
2021-09-03 00:13:07 +0300

    Add files via upload

---
+++ NamuDarbai4/NamuDarbai4Page.cs
@@ -0,0 +1,84 @@
+﻿
+using System.Linq;
+using autotests.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestPageTemplate.Page
+{
+    public class NamuDarbai4Page : BasePage
+    {
+        private const string AddressUrl = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+        private SelectElement dropdown => new SelectElement(Driver.FindElement(By.Id("cars")));
+        private IWebElement resultElement => Driver.FindElement(By.CssSelector("body > div.w3-container.w3-large.w3-border"));
+        private IWebElement submitButton => Driver.FindElement(By.CssSelector("body > form > input[type=submit]"));
+        private IWebElement runButton => Driver.FindElement(By.CssSelector("#runbtn"));
+
+        private IWebElement acceptPrivacyPolicyButton => Driver.FindElement(By.Id("accept-choices"));
+
+        public NamuDarbai4Page(IWebDriver webdriver) : base(webdriver) { }
+
+        public void NavigateToPage()
+        {
+            if (Driver.Url != AddressUrl)
+            {
+                Driver.Url = AddressUrl;
+            }
+        }
+
+        public void AcceptCookies()
+        {
+            try
+            {
+                acceptPrivacyPolicyButton.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                //do nothing, there is no cookies window
+            }
+        }
+
+        public void SelectCarModelFromDropdown(string[] CarModelsArray)
+        {
+            var CarModels = CarModelsArray.ToList();
+            Driver.SwitchTo().Frame("iframeResult");
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.Control);
+            foreach (IWebElement selectCarModel in dropdown.Options)
+            {
+                if (CarModels.Contains(selectCarModel.Text))
+                {
+                    dropdown.SelectByText(selectCarModel.Text);
+                }
+            }
+            action.KeyUp(Keys.Control);
+            action.Build().Perform();
+        }
+
+        public void ClickSubmitButton()
+        {
+            submitButton.Click();
+        }
+
+        public void VerifyResult(string[] SelectedCarModels)
+        {
+            Driver.SwitchTo().Window(Driver.WindowHandles.Last());
+            Driver.SwitchTo().Frame("iframeResult");
+
+            foreach (string selectedCar in SelectedCarModels)
+            {
+                Assert.IsTrue(resultElement.Text.Contains(selectedCar), "Selected option is : " + selectedCar,
+                resultElement.Text, "Selected car is wrong");
+            }
+        }
+
+
+        public void RefreshWebPage()
+        {
+            Driver.SwitchTo().DefaultContent();
+            runButton.Click();
+        }
+    }
+}
---
+++ NamuDarbai4/NamuDarbai4Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TestPageTemplate.Page;
+
+namespace TestPageTemplate.Test
+{
+    public class NamuDarbai4Test
+    {
+        private static NamuDarbai4Page page;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            page = new NamuDarbai4Page(chrome);
+            chrome.Manage().Window.Maximize();
+            chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            page.CloseBrowser();
+        }
+
+        [TestCase(new string[] { "Volvo" }, new string[] { "volvo" }, TestName = "Test first selected with Volvo")]
+        [TestCase(new string[] { "Volvo", "Audi" }, new string[] { "volvo","audi" }, TestName = "Test second selected with Volvo and Audi")]
+        [TestCase(new string[] { "Volvo", "Audi", "Saab", "Opel" }, new string[] { "volvo","audi","saab","opel" }, TestName = "Test second selected with Volvo, Audi, Saab and Opel")]
+        public static void TestCarSelection(string[] CarModels, string[] SelectedCarModels)
+        { 
+            page.NavigateToPage();
+            page.AcceptCookies();
+            page.RefreshWebPage();
+            //page.GetWait(5);
+            page.SelectCarModelFromDropdown(CarModels);
+            page.ClickSubmitButton();
+            page.GetWait(5);
+            page.VerifyResult(SelectedCarModels);
+        }
+    }
+}
